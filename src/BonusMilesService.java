2fa61cdc02f97486f2c9d1dd3235fcf064678f55
Anastasia <a.grebenkina93@gmail.com>
2022-10-11 10:36:28 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int result;
+        int bonus = 20;
+        result = price / bonus;
+        return result;
+    }
+}
